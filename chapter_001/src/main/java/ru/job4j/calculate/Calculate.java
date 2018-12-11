dc1b5fc8ea86878156e895b1a87d511875a3247b
Viktor Shiyan <VictorShyan@yandex.ru>
2018-12-11 13:54:45 +0600

    Изменение версии компилятора в maven

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -22,7 +22,7 @@ public class Calculate {
      * @param name Your name.
      * @return Echo plus your name.
      */
-    public String echo(String name) {
+    String echo(String name) {
         return "Echo, echo, echo : " + name;
     }
 }
