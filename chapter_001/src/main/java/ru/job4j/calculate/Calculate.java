ca8d858bae5f5b0beb7d012e1fd74435911c9831
Viktor Shiyan <VictorShyan@yandex.ru>
2018-12-11 10:09:58 +0600

    1.3. JavaDoc. Code convention[#95996]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ * Класс служит для вывода приветствия
+ * @author Viktor Shiyan
+ * @since 11.12.2018
+ */
 public class Calculate {
+    /**
+     * Главный метод
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
