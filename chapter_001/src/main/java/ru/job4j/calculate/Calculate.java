dabad71caa109d577b1fbf21ad2caff9b00b2536
Viktor Shiyan <VictorShyan@yandex.ru>
2018-12-11 10:23:41 +0600

    1.3.1 Maven Plugins[#95993]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,13 +2,15 @@ package ru.job4j.calculate;
 
 /**
  * Класс служит для вывода приветствия
+ *
  * @author Viktor Shiyan
  * @since 11.12.2018
  */
 public class Calculate {
     /**
      * Главный метод
-     * @param args
+     *
+     * @param args аргументы командной строки
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
