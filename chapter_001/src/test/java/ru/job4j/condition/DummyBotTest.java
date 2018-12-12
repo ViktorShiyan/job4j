44034470818c39a8db981df41a6d9b8b2dbaaf9e
Viktor Shiyan <VictorShyan@yandex.ru>
2018-12-12 22:11:17 +0600

    4.1. Глупый бот.[#95959]

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for class DummyBot
+ *
+ * @author Viktor Shiyan
+ * @since 12.12.2018
+ */
+public class DummyBotTest {
+
+    /**
+     * Тест приветствия
+     */
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    /**
+     * Тест прощания
+     */
+    @Test
+    public void whenByuBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    /**
+     * Тест неизвестного вопроса
+     */
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Спросите другой вопрос.")
+        );
+    }
+}
