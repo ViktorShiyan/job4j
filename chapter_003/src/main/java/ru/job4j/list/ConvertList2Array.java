3526e7175b289bad4af9174891a88302180f0db9
vshiyan <vshiyan.py@yandex.ru>
2019-01-05 22:26:07 +0600

    1. Конвертация листа массивов в один лист Integer[#96025]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,5 +1,6 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +10,13 @@ import java.util.List;
  * @since 05.01.2019
  */
 public class ConvertList2Array {
+    /**
+     * Метод рабивающий список на двумерный массив
+     *
+     * @param list входной список
+     * @param rows количество массивов
+     * @return двумерный массив
+     */
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = (int) Math.ceil((double) list.size() / (double) rows);
         int count = 0;
@@ -16,7 +24,6 @@ public class ConvertList2Array {
         for (int out = 0; out < rows; out++) {
             for (int in = 0; in < cells; in++) {
                 if (count >= list.size()) {
-                    array[out][in] = 0;
                     count++;
                 } else {
                     array[out][in] = list.get(count);
@@ -25,5 +32,21 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    /**
+     * Метод объединяет в один список значения из списка массивов
+     *
+     * @param list входной список массивов со значениями
+     * @return список
+     */
+    public List<Integer> convert(List<int[]> list) {
+        ArrayList<Integer> listResult = new ArrayList<Integer>();
+        for (int[] arr : list) {
+            for (int item : arr) {
+                listResult.add(item);
+            }
+        }
+        return listResult;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -2,7 +2,10 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -35,6 +38,34 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {7, 8, 9}
         };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when2X2Array() {
+        ConvertList2Array list2Array = new ConvertList2Array();
+        List<int[]> listArrayInt = new ArrayList<>();
+        listArrayInt.add(new int[]{1, 2});
+        listArrayInt.add(new int[]{3, 4});
+        List<Integer> result = list2Array.convert(listArrayInt);
+        List<Integer> expect = new ArrayList<Integer>();
+        for (int i = 1; i < 5; i++) {
+            expect.add(i);
+        }
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when2And6Array() {
+        ConvertList2Array list2Array = new ConvertList2Array();
+        List<int[]> listArrayInt = new ArrayList<>();
+        listArrayInt.add(new int[]{1, 2});
+        listArrayInt.add(new int[]{3, 4, 5, 6, 7, 8});
+        List<Integer> result = list2Array.convert(listArrayInt);
+        List<Integer> expect = new ArrayList<Integer>();
+        for (int i = 1; i < 9; i++) {
+            expect.add(i);
+        }
         assertThat(result, is(expect));
     }
 }
