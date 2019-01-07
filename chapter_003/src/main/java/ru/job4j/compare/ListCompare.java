b0b486d4d8be6ccaaa77cf8f90b08773d3986791
ViktorShiyan <VictorShyan@yandex.com>
2019-01-07 16:20:30 +0900

    3. Компаратор для строк.[#96010]

---
+++ chapter_003/src/main/java/ru/job4j/compare/ListCompare.java
@@ -0,0 +1,29 @@
+package ru.job4j.compare;
+
+import java.util.Comparator;
+
+/**
+ * Компаратор для сравнения строк
+ *
+ * @author Viktor Shiyan
+ * @since 07.01.2019
+ */
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        int litleLenght = left.length() < right.length() ? left.length() : right.length();
+        for (int i = 0; i < litleLenght; i++) {
+            if (left.charAt(i) > right.charAt(i)) {
+                result = 1;
+                break;
+            } else if (left.charAt(i) < right.charAt(i)) {
+                result = -1;
+            }
+        }
+        if (result == 0 && left.length() != right.length()) {
+            result = left.length() > right.length() ? 1 : -1;
+        }
+        return result;
+    }
+}
