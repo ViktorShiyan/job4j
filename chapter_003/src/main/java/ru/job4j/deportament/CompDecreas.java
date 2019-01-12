0ec20e67ce615d7cb4cfa71d48849ae4935496b8
Viktor Shiyan <VictorShyan@yandex.com>
2019-01-12 18:03:05 +0900

    Изменение компаратора для депортаментов

--- chapter_003/src/main/java/ru/job4j/deportament/CompDecreas.java
+++ chapter_003/src/main/java/ru/job4j/deportament/CompDecreas.java
@@ -12,25 +12,14 @@ public class CompDecreas implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        char[] chars1 = o1.toCharArray();
-        char[] chars2 = o2.toCharArray();
-        int res = 0;
-        for (int i = 0; i < chars1.length && i < chars2.length && !o1.equals(o2); i++) {
-            char s1 = chars1[i];
-            char s2 = chars2[i];
-            if (s1 > s2) {
-                res = -1;
-                break;
-            } else if (s1 < s2) {
-                res = 1;
-                break;
-            }
+        int result;
+        if (o2.startsWith(o1)) {
+            result = -1;
+        } else if (o1.startsWith(o2)) {
+            result = 1;
+        } else {
+            result = o2.compareTo(o1);
         }
-        if (res == 0 && o1.length() > o2.length()) {
-            res = 1;
-        } else if (res == 0 && o1.length() < o2.length()) {
-            res = -1;
-        }
-        return res;
+        return result;
     }
 }
