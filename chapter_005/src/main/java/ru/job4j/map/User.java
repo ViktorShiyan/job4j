cf482f6995db23089fc9d9bcbc09aebcd6a1cd24
Viktor Shiyan <VictorShyan@yandex.ru>
2019-01-23 11:29:19 +0900

    Класс USer with hash and equals

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 /**
  * Класс описывающий пользователя
@@ -9,9 +11,19 @@ import java.util.Calendar;
  * @since 22.01.2019
  */
 public class User {
+    private int hashCode;
     private String name;
     private int children;
     private Calendar birthday;
+
+    public static void main(String[] args) {
+        User userOne = new User("Viktor", 1, new GregorianCalendar(1991, 3, 2));
+        User userTwo = new User("Viktor", 1, new GregorianCalendar(1991, 3, 2));
+        HashMap<User, String> hashMap = new HashMap<>();
+        hashMap.put(userOne, "First");
+        hashMap.put(userTwo, "Second");
+        System.out.println(hashMap);
+    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -19,15 +31,27 @@ public class User {
         this.birthday = birthday;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof User)) {
+            return false;
+        }
+        User uComp = (User) obj;
+        return uComp.name.equals(this.name) && uComp.children == this.children && uComp.birthday.equals(this.birthday);
+
     }
 
-    public int getChildren() {
-        return children;
-    }
-
-    public Calendar getBirthday() {
-        return birthday;
+    @Override
+    public int hashCode() {
+        int result = this.hashCode;
+        if (result == 0) {
+            result = 31 * result + this.birthday.hashCode();
+            result = 31 * result + this.name.hashCode();
+            result = 31 * result + this.children;
+        }
+        return result;
     }
 }
