00b32728ffcdf97dcc50d6aafca81fdf19595856
Viktor Shiyan <VictorShyan@yandex.ru>
2019-01-25 11:18:18 +0900

    1. Создать элементарную структуру дерева[#106121]

--- chapter_005/src/main/java/ru/job4j/tree/Node.java
+++ chapter_005/src/main/java/ru/job4j/tree/Node.java
@@ -1,4 +1,55 @@
 package ru.job4j.tree;
 
-public class Node {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс описывающий ноду дерева
+ *
+ * @author Viktor Shiyan
+ * @since 25.01.2019
+ */
+public class Node<E extends Comparable<E>> {
+    private final List<Node<E>> children = new ArrayList<>();
+    private final E value;
+
+    public Node(final E value) {
+        this.value = value;
+    }
+
+    /**
+     * Метод добавляющий дочерний элемент в ноду
+     *
+     * @param child добавленный элемент
+     */
+    public void add(Node<E> child) {
+        this.children.add(child);
+    }
+
+    /**
+     * Метод возвращающий список дочерних элементов
+     *
+     * @return
+     */
+    public List<Node<E>> leaves() {
+        return this.children;
+    }
+
+    /**
+     * Метод для получения значения
+     *
+     * @return значение
+     */
+    public E getValue() {
+        return this.value;
+    }
+
+    /**
+     * @param that элемент
+     * @return равны ли элементы
+     */
+    public boolean eqValue(E that) {
+        return this.value.compareTo(that) == 0;
+
+    }
 }
--- chapter_005/src/main/java/ru/job4j/tree/SimpleTree.java
+++ chapter_005/src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,4 +1,28 @@
 package ru.job4j.tree;
 
-public class SimpleTree {
+import java.util.Optional;
+
+/**
+ * Интерфейс для реализации дерева
+ *
+ * @param <E> тип хранимых элементов
+ */
+public interface SimpleTree<E extends Comparable<E>> extends Iterable<E> {
+    /**
+     * Добавить элемент child в parent.
+     * Parent может иметь список child.
+     *
+     * @param parent parent.
+     * @param child  child.
+     * @return удалось ли добавить
+     */
+    boolean add(E parent, E child);
+
+    /**
+     * Производит поиск узла по заданному значению.
+     *
+     * @param value заданное значение.
+     * @return искомый узел.
+     */
+    Optional<Node<E>> findBy(E value);
 }
