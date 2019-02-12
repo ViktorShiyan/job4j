44c70e7320498fc34fa1f40aef1393fa4bf9beaf
ViktorShiyan <VictorShyan@yandex.ru>
2019-02-12 20:23:06 +0600

    5. Создать программу консольный чат. [#106159]

---
+++ chapter_006/src/main/java/ru/job4j/chatbot/Bot.java
@@ -0,0 +1,86 @@
+package ru.job4j.chatbot;
+
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Чат бот отвечающий случайной фразой из файла
+ *
+ * @author ViktorShiyan
+ * @since 12.02.2019
+ */
+public class Bot {
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        bot.dialog();
+    }
+
+    /**
+     * Метод для ведения диалога с ботом
+     */
+    public void dialog() {
+        boolean run = true;
+        boolean mute = false;
+        String ask;
+        Scanner scanner = new Scanner(System.in);
+        while (run) {
+            ask = scanner.nextLine();
+            if (ask.equals("закончить")) {
+                log(ask);
+                run = false;
+            } else if (ask.equals("стоп")) {
+                log(ask);
+                mute = true;
+            } else if (ask.equals("продолжить")) {
+                log(ask);
+                mute = false;
+            } else {
+                log(ask);
+                if (!mute) {
+                    System.out.println(botAnswer(new File("C:\\answer.txt")));
+                }
+            }
+        }
+    }
+
+    /**
+     * Метод возвращает случайный ответ бота из файла
+     *
+     * @return строка с ответом
+     */
+    private String botAnswer(File f) {
+        String result = null;
+        try {
+            Random rand = new Random();
+            int n = 0;
+            for (Scanner sc = new Scanner(f); sc.hasNext();) {
+                ++n;
+                String line = sc.nextLine();
+                if (rand.nextInt(n) == 0) {
+                    result = line;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Метод для записи в файл лога
+     *
+     * @param logText записываеммые слова в лог
+     */
+    private void log(String logText) {
+        try (FileWriter writer = new FileWriter("C:\\logBot.txt", true)) {
+            writer.write(logText + System.lineSeparator());
+            writer.flush();
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
